ee34bb9593e22467d4eecafc58a9d4371aa6d7b4
emman <emman@MANUEGI12>
2025-04-10 22:12:48 -0300

    Adicionar arquivos de projeto.

---
+++ Thiago_Aula2/Class1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Thiago_Aula2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string caminhoArquivo = "nomes.txt";
+
+            // Se quiser chamar AdicionarNome como método estático:
+            Class1.AdicionarNome(caminhoArquivo);
+
+            // Criar a instância para chamar o método não estático
+            Class1 minhaClasse = new Class1();
+            minhaClasse.ListarNomes(caminhoArquivo);
+        }
+    }
+{
+    public class Class1
+    {
+
+        static void AdicionarNome(string caminhoArquivo)
+        {
+            Console.Write("Digite um nome: ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Digite a data : ");
+            DateTime date = Convert.ToDateTime(Console.ReadLine());
+
+            // Adiciona o nome no arquivo (modo append)
+            File.AppendAllText(caminhoArquivo, nome + ";" + date.ToString("dd/MM/yyyy") + Environment.NewLine);
+
+            Console.WriteLine("Nome e data salvos com sucesso!");
+        }
+
+        public void ListarNomes(string caminhoArquivo)
+        {
+            Console.WriteLine("\n--- Lista de nomes persistidos ---");
+
+            if (File.Exists(caminhoArquivo))
+            {
+                string[] arquivo = File.ReadAllLines(caminhoArquivo);
+
+                foreach (string nome in arquivo)
+                {
+                    Console.WriteLine("- " + nome);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Nenhum nome foi salvo ainda.");
+            }
+        }
+
+    }
+}
---
+++ Thiago_Aula2/Program.cs
@@ -0,0 +1,32 @@
+﻿string caminhoArquivo = "dados.txt";
+
+Console.WriteLine("=== EXEMPLO DE PERSISTÊNCIA DE DADOS ===");
+
+PersistenciaDados persistencia = new PersistenciaDados();
+
+persistencia.CaminhoArquivo = "Thiago_Aula2.txt";
+
+while (true)
+{
+    Console.WriteLine("\n1 - Adicionar nome");
+    Console.WriteLine("2 - Listar nomes");
+    Console.WriteLine("0 - Sair");
+    Console.Write("Escolha uma opção: ");
+    string opcao = Console.ReadLine();
+
+    switch (opcao)
+    {
+        case "1":
+            AdicionarNome(caminhoArquivo);
+            break;
+        case "2":
+            ListarNomes(caminhoArquivo);
+            break;
+        case "0":
+            Console.WriteLine("Encerrando o programa.");
+            return;
+        default:
+            Console.WriteLine("Opção inválida.");
+            break;
+    }
+}
